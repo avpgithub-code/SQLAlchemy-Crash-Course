f694f50edd6007a6f8a26570bd702df0be4dcf84
Archit Pandya <architpandya@yahoo.com>
2025-11-27 08:40:31 -0500

    Annotated DeclarativeBase

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_user1 = User(name="Ben", age=66)
-new_user2 = User(name="Bob", age=34)
-new_user3 = User(name="Kevin", age=76)
-new_user4 = User(name="John", age=56)
-new_user5 = User(name="Rambo", age=66)
+new_user1 = User(first_name="Ben",last_name="Smith",age=66)
+new_user2 = User(first_name="Bob",last_name="Smith",age=34)
+new_user3 = User(first_name="Kevin",last_name="Smith",age=76)
+new_user4 = User(first_name="John",last_name="Smith",age=56)
+new_user5 = User(first_name="Rambo",last_name="Smith",age=66)
 
 # session.add(new_user)
 session.add_all([new_user1, new_user2, new_user3, new_user4, new_user5])
@@ -18,10 +18,10 @@ print(f"Added new users with IDs: {new_user1.id}, {new_user2.id}, {new_user3.id}
 # Verify by querying the database
 users = session.query(User).all()
 for user in users:
-    print(f"User ID: {user.id}, Name: {user.name}, Age: {user.age}")
+    print(f"User ID: {user.id}, Name: {user.first_name}, Age: {user.age}")
 
 # Update a user
-user_to_update = session.query(User).filter_by(name="Bob").first()
+user_to_update = session.query(User).filter_by(first_name="Bob").first()
 if user_to_update:
     user_to_update.age = 99
     session.commit()
--- models.py
+++ models.py
@@ -1,21 +1,48 @@
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, declarative_base, sessionmaker, mapped_column, Mapped
+from typing import Optional
+from typing_extensions import Annotated
 
 # SQLite engine URL format:
 sqlite_db_url = 'sqlite:///mydbsqlite.db'
 engine = create_engine(sqlite_db_url, echo=True)
 
-Base = declarative_base()
-Base.metadata.create_all(engine)
+str_20 = Annotated[Optional[str], mapped_column(String(20))]
+str_30 = Annotated[Optional[str], mapped_column(String(30))]
 
+# Define base class for declarative models
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        int: Integer,
+        str: String,
+    }
+
+# Define User model
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    age = Column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    first_name: Mapped[str_20]
+    last_name: Mapped[str_30]
+    age: Mapped[int] = mapped_column()
 
+# Create all tables in the database
 Base.metadata.create_all(engine)
 
+# class User(Base):
+#     __tablename__ = 'users'
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     name: Mapped[Optional[str]] = mapped_column()
+#     age: Mapped[int] = mapped_column()
+
+
+
+# from sqlalchemy import Column, Integer, String, create_engine
+# Base = declarative_base()
+# class User(Base):
+#     __tablename__ = 'users'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     name = Column(String, nullable=False)
+#     age = Column(Integer, nullable=False)
 
 # Session = sessionmaker(bind=engine)
 # session = Session()
